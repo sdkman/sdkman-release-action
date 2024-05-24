2e794411463da2e05fe234f6a1e1d8c339300ae0
Hamza REMMAL <hamza@remmal.dev>
2024-05-24 23:21:44 +0200

    Add checksums as inputs

--- src/main.js
+++ src/main.js
@@ -11,11 +11,46 @@ async function main() {
   const url = core.getInput("URL");
   const backend = core.getInput("BACKEND");
 
+  // EXTRACT CHECKSUMS INPUTS
+  const checksum_md5 = core.getInput("CHECKSUM-MD5");
+  const checksum_sha1 = core.getInput("CHECKSUM-SHA-1");
+  const checksum_sha224 = core.getInput("CHECKSUM-SHA-224");
+  const checksum_sha256 = core.getInput("CHECKSUM-SHA-256");
+  const checksum_sha384 = core.getInput("CHECKSUM-SHA-384");
+  const checksum_sha512 = core.getInput("CHECKSUM-SHA-512");
+
+  const checksum_payload = {};
+
+  if (checksum_md5) {
+    checksum_payload["MD5"] = checksum_md5;
+  }
+
+  if (checksum_sha1) {
+    checksum_payload["SHA-1"] = checksum_sha1;
+  }
+
+  if (checksum_sha224) {
+    checksum_payload["SHA-224"] = checksum_sha224;
+  }
+
+  if (checksum_sha256) {
+    checksum_payload["SHA-256"] = checksum_sha256;
+  }
+
+  if (checksum_sha384) {
+    checksum_payload["SHA-384"] = checksum_sha384;
+  }
+
+  if (checksum_sha512) {
+    checksum_payload["SHA-512"] = checksum_sha512;
+  }
+
   const payload = {
     candidate: candidate,
     version: version,
     platform: platform,
     url: url,
+    checksums: checksum_payload,
   };
 
   const query_config = {
